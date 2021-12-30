c88c11a99a5dff913f57ad06acc1d49b4c3a7942
RealMepon <95572797+RealMepon@users.noreply.github.com>
2021-12-30 14:48:24 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,7 @@
-python -m pip install -r requirements.txt
+pip install requests
+pip install time
+pip install discord
+pip install random
+pip install string
+pip install slowprint
+pip install colorama
